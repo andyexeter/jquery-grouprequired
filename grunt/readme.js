060cc6a923f12ee66ce48c7aff1d60224fa3d3a3
andyexeter <palmer.andy@gmail.com>
2016-12-22 10:11:37 +0000

    Code cleanup

--- grunt/readme.js
+++ grunt/readme.js
@@ -5,39 +5,34 @@ module.exports = function( grunt ) {
 		var data = {
 			pkg: getProcessedJSON( 'package.json' ),
 			bwr: getProcessedJSON( 'bower.json' ),
-			files: {}
+			files: {
+				main: getFile( '<%= files.main %>' ),
+				min: getFile( '<%= files.min %>' )
+			}
 		};
 
-		data.files.main = getFile( 'dist/jquery.grouprequired.js' );
-		data.files.min = getFile( 'dist/jquery.grouprequired.min.js' );
-
-		var tpl = grunt.template.process( grunt.file.read( 'src/README.tpl.md' ), { data: data } );
-
-		grunt.file.write( 'README.md', tpl );
+		grunt.file.write( 'README.md', grunt.template.process( grunt.file.read( 'src/README.tpl.md' ), { data: data } ) );
 	} );
 
 	function getProcessedJSON( filepath, data ) {
-		var contents = grunt.file.read( filepath );
-
-		var processed = grunt.template.process( contents, data );
-
-		return JSON.parse( processed );
+		return JSON.parse( grunt.template.process( grunt.file.read( filepath ), data ) );
 	}
 
 	var path = require( 'path' );
 	var zlib = require( 'zlib' );
 
 	function getFile( filepath ) {
+		filepath = grunt.template.process( filepath );
 		var contents = grunt.file.read( filepath );
 
 		return {
 			name: path.basename( filepath ),
-			size: humanFileSize( contents.length ),
-			gzipped: humanFileSize( zlib.gzipSync( contents, { level: 9 } ).length )
+			size: formatSize( contents.length ),
+			gzipped: formatSize( zlib.gzipSync( contents, { level: 9 } ).length )
 		};
 	}
 
-	function humanFileSize( size ) {
+	function formatSize( size ) {
 		var i = Math.floor( Math.log( size ) / Math.log( 1024 ) );
 		return ( size / Math.pow( 1024, i ) ).toFixed( 2 ) * 1 + '' + [ 'B', 'kB', 'MB', 'GB', 'TB' ][ i ];
 	}
