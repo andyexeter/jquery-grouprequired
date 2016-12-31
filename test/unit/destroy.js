ba6d8396feb818d1858f6bda8db1f5e42103d093
andyexeter <palmer.andy@gmail.com>
2016-12-31 11:22:10 +0000

    Fix for grunt-contrib-qunit so PhantomJS fires invalid events

--- test/unit/destroy.js
+++ test/unit/destroy.js
@@ -24,7 +24,13 @@ QUnit.test( 'destroy method should remove all traces of plugin instance', functi
 
 		$inputs.groupRequired( 'destroy' );
 
-		$form.find( '[type=submit]' ).click();
+		/*
+		 * Fix for grunt-contrib-qunit. PhantomJS doesn't appear to call checkValidity() before
+		 * submitting the form so the 'invalid' event never fires.
+		 */
+		if ( $form.get( 0 ).checkValidity() ) {
+			$form.find( '[type=submit]' ).click();
+		}
 	} );
 
 } );
