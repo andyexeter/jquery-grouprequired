1a7969214e107766f37dd56fbd48efaab066e179
andyexeter <palmer.andy@gmail.com>
2017-01-01 16:17:40 +0000

    Add unit test wrapper method

--- test/unit/checkboxes.js
+++ test/unit/checkboxes.js
@@ -1,31 +1,10 @@
-/* global QUnit:false, $:false */
-QUnit.test( 'form with checkboxes should submit if one checkbox is checked.', function( assert ) {
+(function( addTest ) {
 	'use strict';
 
-	var done = assert.async();
-
-	$( function() {
-		var $form   = $( '#checkboxes' ),
-			$inputs = $form.find( 'input[type=checkbox]' );
-
-		$inputs.prop( 'checked', false ).last().prop( 'checked', true );
-
-		$inputs.groupRequired();
-
-		$inputs.eq( -1 ).one( 'invalid', function() {
-			assert.ok( false, 'Form was not submitted.' );
-			done();
-		} );
-
-		$form.one( 'submit', function( e ) {
-			e.preventDefault();
-
-			assert.ok( true, 'Form was submitted.' );
-			done();
-		} );
-
-		$form.find( '[type=submit]' ).click();
+	addTest( 'form with checkboxes should submit if one checkbox is checked', {
+		form: '#checkboxes',
+		setup: function( $inputs ) {
+			$inputs.prop( 'checked', false ).last().prop( 'checked', true );
+		}
 	} );
-
-
-} );
+})( $.fn.groupRequired.addUnitTest );
--- test/unit/destroy.js
+++ test/unit/destroy.js
@@ -1,36 +1,20 @@
-/* global QUnit:false, $:false */
-QUnit.test( 'destroy method should remove all traces of plugin instance', function( assert ) {
+(function( addTest ) {
 	'use strict';
 
-	var done = assert.async();
-
-	$( function() {
-		var $form   = $( '#destroy' ),
-			$inputs = $form.find( 'input[type=tel]' );
-
-		$inputs.val( '' ).groupRequired();
-
-		$inputs.eq( -1 ).one( 'invalid', function() {
+	addTest( 'destroy method should remove all traces of plugin instance', {
+		form: '#destroy',
+		setup: function( $inputs ) {
+			$inputs.eq( 0 ).val( '123456' );
+		},
+		afterSetup: function( $inputs ) {
+			$inputs.groupRequired( 'destroy' );
+			$inputs.prop( 'required', true );
+		},
+		onInvalid: function( assert ) {
 			assert.ok( true, 'Form was not submitted.' );
-			done();
-		} );
-
-		$form.one( 'submit', function( e ) {
-			e.preventDefault();
-
+		},
+		onSubmit: function( assert ) {
 			assert.ok( false, 'Form was submitted.' );
-			done();
-		} );
-
-		$inputs.groupRequired( 'destroy' );
-
-		/*
-		 * Fix for grunt-contrib-qunit. PhantomJS doesn't appear to call checkValidity() before
-		 * submitting the form so the 'invalid' event never fires.
-		 */
-		if ( $form.get( 0 ).checkValidity() ) {
-			$form.find( '[type=submit]' ).click();
 		}
 	} );
-
-} );
+})( $.fn.groupRequired.addUnitTest );
