7de138b98f16fe8742f64830f7f940d687390959
andyexeter <palmer.andy@gmail.com>
2018-05-28 17:08:36 +0100

    Add assertion that form does not submit when both fields are empty

--- test/unit/two-fields.js
+++ test/unit/two-fields.js
@@ -7,4 +7,14 @@
             $inputs.eq(0).val('123456');
         }
     });
+
+    addTest('form with telephone inputs should not submit if both fields are empty', {
+        form: '#first',
+        setup: function ($inputs) {
+            $inputs.val('');
+        },
+        onInvalid: function (assert) {
+            assert.ok(true, 'Form was not submitted.');
+        }
+    });
 })($.fn.groupRequired.addUnitTest);
